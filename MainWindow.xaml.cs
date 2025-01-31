06fd37569b33231300f88f8a506145d66e6fc804
Rick <ricardobriangarcia@gmail.com>
2025-01-31 00:48:18 -0300

    First Version
    First Version

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,5 +24,41 @@ namespace ProjetaUpdate
         {
             InitializeComponent();
         }
+
+        private void BorderArrasto_Pressionado(object sender, MouseButtonEventArgs e)
+        {
+            // Verifica se o botão esquerdo do mouse está pressionado
+            if (e.ButtonState == MouseButtonState.Pressed)
+            {
+                // Permite arrastar a janela
+                this.DragMove();
+            }
+        }
+
+        private void FecharApp(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void ProjetaHDR_Checked(object sender, RoutedEventArgs e)
+        {
+            this.ProjetaHDRContent.Visibility = Visibility.Visible;
+        }
+
+        private void ProjetaHDR_Unchecked(object sender, RoutedEventArgs e)
+        {
+            this.ProjetaHDRContent.Visibility = Visibility.Hidden;
+        }
+
+        private void BotaoProjetaHDR_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            var botao = sender as RadioButton;
+            if (botao != null && botao.IsChecked == true)
+            {
+                botao.IsChecked = false;
+                e.Handled = true; // Previne o comportamento padrão do RadioButton
+            }
+
+        }
     }
 }
