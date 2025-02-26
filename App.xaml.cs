3e6091955df6781dfd627fb00cfbd1f7204277a0
Ricardo Garcia Braga <ricardobriangarcia@gmail.com>
2025-02-26 12:44:42 -0300

    1.0.0

--- App.xaml.cs
+++ App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using Squirrel;
 
 namespace ProjetaUpdate
 {
@@ -13,5 +14,39 @@ namespace ProjetaUpdate
     /// </summary>
     public partial class App : Application
     {
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            bool updated = await CheckForUpdatesAtStartup();
+        }
+
+        private async Task<bool> CheckForUpdatesAtStartup()
+        {
+            try
+            {
+                using (var updateManager = await UpdateManager.GitHubUpdateManager(@"https://github.com/gbragaricardo/ProjetaUpdate"))
+                {
+                    var updateInfo = await updateManager.CheckForUpdate();
+
+                    if (updateInfo.ReleasesToApply.Count > 0)
+                    {
+                        MessageBox.Show("Nova versão disponível! O aplicativo será atualizado e reiniciado.", "Atualização Disponível", MessageBoxButton.OK, MessageBoxImage.Information);
+
+                        await updateManager.UpdateApp();
+
+                        // Reinicia o aplicativo após a atualização
+                        UpdateManager.RestartApp();
+                        return true;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Erro ao buscar atualizações: {ex.Message}", "Erro de Atualização", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+            return false; // Nenhuma atualização foi aplicada
+        }
     }
 }
