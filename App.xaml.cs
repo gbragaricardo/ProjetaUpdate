309e955dd59c0f4bb657a8ebe7077a9b90a0c00d
Ricardo Garcia Braga <ricardobriangarcia@gmail.com>
2025-02-26 18:22:24 -0300

    1.0.4
    Update Test

--- App.xaml.cs
+++ App.xaml.cs
@@ -25,11 +25,9 @@ namespace ProjetaUpdate
 
         private async Task<bool> CheckForUpdatesAtStartup()
         {
+
             try
             {
-                string desktopShortcut = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "ProjetaUpdater.lnk");
-                string iconPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Resources", "Setup.ico");
-
                 using (var updateManager = await UpdateManager.GitHubUpdateManager(@"https://github.com/gbragaricardo/ProjetaUpdate"))
                 {
                     var updateInfo = await updateManager.CheckForUpdate();
@@ -39,14 +37,13 @@ namespace ProjetaUpdate
                         MessageBox.Show("Nova versão disponível! O aplicativo será atualizado e reiniciado.", "Atualização Disponível", MessageBoxButton.OK, MessageBoxImage.Information);
 
                         await updateManager.UpdateApp();
-
-                        SetupShortcut();
-
                         UpdateManager.RestartApp();
+                        SetupShortcut();
                         return true;
                     }
                 }
             }
+
             catch (Exception ex)
             {
                 MessageBox.Show($"Erro ao buscar atualizações: {ex.Message}", "Erro de Atualização", MessageBoxButton.OK, MessageBoxImage.Error);
@@ -57,11 +54,12 @@ namespace ProjetaUpdate
 
         private static void SetupShortcut()
         {
-            string exePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Projeta\nUpdate.exe");
+            string exePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "ProjetaUpdate.exe");
             string iconPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Resources", "Setup.ico");
 
             // Criar atalho com nome formatado "Projeta\nUpdate"
-            ShortcutHelper.CreateShortcut("Projeta Update", exePath, iconPath);
+            ShortcutHelper.CreateShortcut("Teste", exePath, iconPath);
         }
+
     }
 }
