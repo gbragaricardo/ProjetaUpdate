26a7721ab5c70278bed82a05c6331e40ddcb0af9
Rick <ricardobriangarcia@gmail.com>
2025-02-27 22:16:18 -0300

    1.0.0 Instant Fix
    Instant Fix

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Windows;
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Projeta Updater")]
+[assembly: AssemblyProduct("Projeta Update")]
 [assembly: AssemblyCopyright("Copyright ©  2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
