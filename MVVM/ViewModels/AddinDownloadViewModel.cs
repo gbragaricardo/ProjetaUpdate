95d6c1fb4d563af999233cebf7c974b9157c5ea7
Ricardo Braga <ricardobriangarcia@gmail.com>
2026-05-28 23:49:24 -0300

    release 1.4.6 limit revit version options

--- MVVM/ViewModels/AddinDownloadViewModel.cs
+++ MVVM/ViewModels/AddinDownloadViewModel.cs
@@ -14,7 +14,7 @@ namespace ProjetaUpdate.MVVM.ViewModels
 
         private readonly VersionService _vService;
         private readonly OnlineVersionService _onlineVService;
-        public List<string> AvailableRevitVersions { get; } = new List<string> { "2026", "2025", "2024", "2023", "2022" };
+        public List<string> AvailableRevitVersions { get; } = new List<string> { "2026", "2024" };
         public IProgress<string> StatusProgress { get; }
 
 
